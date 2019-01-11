23fe4b0c47d8fe9e31ecd91e4be4cd7548a878d1
Harajuku Girl <n.al-wattar@kew.org>
2019-01-11 10:03:21 +0000

    Jan 10

---
+++ MSArunner.py
@@ -0,0 +1,44 @@
+import sys 
+import Bio.SeqIO
+import csv
+import subprocess
+import Bio.SeqRecord
+import unittest
+import Bio.AlignIO
+
+class MultipleSequenceAlignmentRunner(object):
+	"""Wrapper class for running Multiple Sequence Alignment (MSA) programs
+	 such as Clustal Omega and MAFFT.
+
+This is a base class for runners that wrap specific MSA programs.
+
+"""
+	def __init__(self):
+		pass
+
+	def align(self, seqRecordList):
+		raise StandardError, 'abstract method'
+
+
+
+
+class MAFFTRunner(MultipleSequenceAlignmentRunner):
+
+	def __init__(self):
+		pass
+
+	def align(self, seqRecordList):
+    	p = subprocess.Popen(['mafft', '--auto', '--reorder', '-'], stdin = subprocess.PIPE, stdout = subprocess.PIPE, universal_newlines = True)
+    	records = list(Bio.SeqIO.parse(p.stdout, 'fasta'))
+
+class ClustaloRunner(MultipleSequenceAlignmentRunner):
+
+	def __init__(self):
+		pass
+
+	def clustalo():
+    mergedSequences = mergeSequencesAndConvertToFasta(sequenceList, fastaFile)
+    p = subprocess.Popen(['clustalo' '-i' mergedSequences], stdin = subprocess.PIPE, stdout = subprocess.PIPE, universal_newlines = True)
+
+
+
