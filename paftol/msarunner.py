1b0a71503f0ead85b8777e52f1d5f0b49fe91393
Jan T Kim <jttkim@gmail.com>
2019-01-11 14:40:43 +0000

    add msarunner test

--- paftol/msarunner.py
+++ paftol/msarunner.py
@@ -1,4 +1,4 @@
-import sys 
+import sys
 import Bio.SeqIO
 import csv
 import subprocess
@@ -6,36 +6,38 @@ import Bio.SeqRecord
 import unittest
 import Bio.AlignIO
 
+
 class MultipleSequenceAlignmentRunner(object):
-	"""Wrapper class for running Multiple Sequence Alignment (MSA) programs
-	 such as Clustal Omega and MAFFT.
+
+    """Wrapper class for running Multiple Sequence Alignment (MSA) programs
+     such as Clustal Omega and MAFFT.
 
 This is a base class for runners that wrap specific MSA programs.
 
 """
-	def __init__(self):
-		pass
 
-	def align(self, seqRecordList):
-		raise StandardError, 'abstract method'
+    def __init__(self):
+        pass
 
-class MAFFTRunner(MultipleSequenceAlignmentRunner):
+    def align(self, seqRecordList):
+        raise StandardError, 'abstract method'
 
-	def __init__(self):
-		pass
 
-	def align(self, seqRecordList):
-		p = subprocess.Popen(['mafft', '--auto', '--reorder', '-'], stdin = subprocess.PIPE, stdout = subprocess.PIPE, universal_newlines = True)
-		records = list(Bio.SeqIO.parse(p.stdout, 'fasta'))
+class MafftRunner(MultipleSequenceAlignmentRunner):
+
+    def __init__(self):
+        pass
+
+    def align(self, seqRecordList):
+        p = subprocess.Popen(['mafft', '--auto', '--reorder', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+        records = list(Bio.SeqIO.parse(p.stdout, 'fasta'))
+
 
 class ClustaloRunner(MultipleSequenceAlignmentRunner):
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def clustalo():
-	mergedSequences = mergeSequencesAndConvertToFasta(sequenceList, fastaFile)
-	p = subprocess.Popen(['clustalo' '-i' mergedSequences], stdin = subprocess.PIPE, stdout = subprocess.PIPE, universal_newlines = True)
-
-
-
+    def clustalo():
+        mergedSequences = mergeSequencesAndConvertToFasta(sequenceList, fastaFile)
+        p = subprocess.Popen(['clustalo' '-i'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
