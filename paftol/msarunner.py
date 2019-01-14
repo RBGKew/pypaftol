cb022b72a036e62cce83563fe481a2c7c772b934
Harajuku Girl <n.al-wattar@kew.org>
2019-01-14 14:41:24 +0000

    makeSubprocess

--- paftol/msarunner.py
+++ paftol/msarunner.py
@@ -22,23 +22,24 @@ This is a base class for runners that wrap specific MSA programs.
     def align(self, seqRecordList):
         raise StandardError, 'abstract method'
 
-
 class MafftRunner(MultipleSequenceAlignmentRunner):
 
     def __init__(self):
         pass
 
     def align(self, seqRecordList):
-        p = subprocess.Popen(['mafft', '--auto', '--reorder', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+        p = subprocess.Popen(['mafft', '--auto', '--reorder', '-'],
+                             stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
         Bio.SeqIO.write(seqRecordList, p.stdin, 'fasta')
         p.stdin.close()
         alignment = Bio.AlignIO.parse(p.stdout, 'fasta')
         return alignment
 
     def makeSubprocess(self, sequenceList):
-       p = subprocess.Popen(stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+       mafftArgv = self.makeMafftArgv(mafftProgram) # databaseFname?)
+       logger.debug('%s', ' '.join(mafftArgv))
+       mafftProcess = subprocess.Popen(stdin=subprocess.PIPE, stdout=subprocess.PIPE)
        pid = os.fork()
-       mafftProcess = 
        if pid == 0:
         mafftProcess.stdout.close()
         for sequence in sequenceList:
@@ -47,6 +48,16 @@ class MafftRunner(MultipleSequenceAlignmentRunner):
         os._exit(0)
         mafftProcess.stdin.close()
         for alignedSequence in alignedSequenceList:
+            alignedSequenceReader.readAlignedSequence()
+        mafftProcess.stdout.close()
+        wPid, wExit = os.waitpid(pid, 0)
+        if pid != wPid:
+            raise StandardError, 'wait returned pid %s (expected %d)' % (wPid, pid)
+        if wExit != 0:
+            raise StandardError, 'wait on forked process returned %d' % wExit
+        mafftReturncode = mafftProcess.wait()
+        if mafftReturncode != 0:
+            raise StandardError, 'process "%s" returned %d' % (' '.join(mafftArgv), mafftReturncode)
 
 
 class ClustaloRunner(MultipleSequenceAlignmentRunner):
@@ -55,5 +66,7 @@ class ClustaloRunner(MultipleSequenceAlignmentRunner):
         pass
 
     def clustalo():
-        mergedSequences = mergeSequencesAndConvertToFasta(sequenceList, fastaFile)
-        p = subprocess.Popen(['clustalo' '-i'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
+        mergedSequences = mergeSequencesAndConvertToFasta(
+            sequenceList, fastaFile)
+        p = subprocess.Popen(
+            ['clustalo' '-i'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, universal_newlines=True)
