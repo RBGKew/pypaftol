b8cf2ac05c9cba8408894e7bfbc72aae00f5f4e9
Paul Bailey <Paul.Bailey@earlham.ac.uk>
2019-04-12 15:52:45 +0100

    added variables

--- paftol/database.py
+++ paftol/database.py
@@ -2,3 +2,7 @@ class PaftolDpDetails(object):
     def __init__(self):
 
         self.dbusername = None
+        self.dbpassword = None
+        self.dbhost = None
+        self.dbname = None
+        
